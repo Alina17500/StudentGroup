e28be5bd06c319bb8b74d98448a54b3216b3bfb8
Alina17500 <90615042+Alina17500@users.noreply.github.com>
2024-05-31 20:13:25 +0300

    Add files via upload

---
+++ ru/vsu/cs/volobueva/Main.java
@@ -0,0 +1,15 @@
+package ru.vsu.cs.volobueva;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            //Class.forName("org.postgresql.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            ConsoleApp consoleApp = new ConsoleApp();
+            consoleApp.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
